f4427a9e91d7e8afc4717bc31421bdcac09c35b1
ksdevops58 <ksomasekhardevops@gmail.com>
2025-06-03 10:59:44 +0000

    Enabled public IP on launch

--- vpc.tf
+++ vpc.tf
@@ -14,8 +14,9 @@ resource "aws_subnet" "ibm_web_sn" {
   vpc_id     = aws_vpc.ibm_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-west-2a"
+  map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "ibmweb-subnet"
+    Name = "ibm-web-subnet"
   }
 }
