349e1f49cb73d752244761c46af735f39a3014ec
ksdevops58 <knsomasekhardevops@gmail.com>
2025-06-03 15:28:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region     = "us-west-2"
+  region     = "us-west-2a"
   access_key = var.aws_access_key
-  secret_key = "var.aws_secret_key"
+  secret_key = var.aws_secret_key
 }
 
