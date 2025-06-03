9840454f446667aa6cfaea5b01afa827865a4732
ksdevops58 <ksomasekhardevops@gmail.com>
2025-06-03 11:07:46 +0000

    added DB subnet

--- vpc.tf
+++ vpc.tf
@@ -20,3 +20,17 @@ resource "aws_subnet" "ibm_web_sn" {
     Name = "ibm-web-subnet"
   }
 }
+
+
+# create subnet for Database servers
+resource "aws_subnet" "ibm_DB_sn" {
+  vpc_id     = aws_vpc.ibm_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = "false"
+
+  tags = {
+    Name = "ibm-DB-subnet"
+  }
+}
+
